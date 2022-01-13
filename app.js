2340dcc95f3e4bfd6e0b28dd8abd1a46e31d5260
hackerhucker <fatihozkul3541@hotmail.com>
2022-01-13 22:57:02 +0100

    last version of challenge

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
-const decrease = document.querySelector(".btn btn1");
-const reset = document.querySelector(".btn btn2");
-const random = document.querySelector(".btn btn3");
-const increase = document.querySelector(".btn btn443");
+const decrease = document.querySelector(".btn.btn1");
+const reset = document.querySelector(".btn.btn2");
+const random = document.querySelector(".btn.btn3");
+const increase = document.querySelector(".btn.btn4");
+let num = Number(document.querySelector(".number").textContent);
 
-// Number assignment between 1 and 100
-// you can change the startNumberif you start with "0"
-let num = getRandomNumber();
-document.querySelector(".number").textContent = num;
-console.log(num);
+// function that decreases the number
 
-// decrease.addEventListener("click", decreaseFunction());
+decrease.addEventListener("click", decreaseFunction);
 
-// function decreaseFunction() {
-//   num -= 1;
-//   document.querySelector(".number").textContent = num;
-// }
-function getRandomNumber() {
-  return Math.floor(Math.random() * 100);
+function decreaseFunction() {
+  num -= 1;
+  document.querySelector(".number").textContent = num;
+}
+
+// reset function
+
+reset.addEventListener("click", resetFunction);
+
+function resetFunction() {
+  num = 0;
+  document.querySelector(".number").textContent = num;
+}
+
+// function that assigns the random number
+
+random.addEventListener("click", getrandomNumber);
+
+function getrandomNumber() {
+  num = Math.floor(Math.random() * 100);
+  document.querySelector(".number").textContent = num;
+}
+
+// function that increases the number
+
+increase.addEventListener("click", increaseFunction);
+
+function increaseFunction() {
+  num += 1;
+  document.querySelector(".number").textContent = num;
 }
